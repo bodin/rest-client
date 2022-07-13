cbf601b1d919bfd197fed7315b9b425be440b84a
Derek Bodin <derek.bodin@gmail.com>
2022-07-12 22:06:19 -0500

    feat: adding byte[]

---
+++ src/main/java/io/bodin/rest/engine/simple/handlers/ByteContentHandler.java
@@ -0,0 +1,19 @@
+package io.bodin.rest.engine.simple.handlers;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+public class ByteContentHandler implements ContentHandler {
+
+    @Override
+    public <T> boolean isSupported(String contentType, Class<T> type) {
+        return type.equals(byte[].class);
+    }
+
+    public <T> T parse(InputStream in, Class<T> type) throws IOException {
+        @SuppressWarnings("unchecked")
+        T result = (T)in.readAllBytes();
+        return result;
+    }
+}
